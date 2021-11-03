9d1719b64036d1f45c66216eeefd2d7192d037c9
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-11-03 14:22:01 -0400

    Cleanup and add readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+## Example of How to get HMR working with dynamic imports
+
+This repo provides a working example of how to work around limitations with Parcel HMR when using dynamic imports.
+
+It is a response to this [stackoverflow question](https://stackoverflow.com/questions/69810228/hmr-with-parcel-js-within-dynamic-imports/69828140#69828140)
--- src/dynamic.js
+++ src/dynamic.js
@@ -1,21 +1,19 @@
+// The init function won't get re-run with parcel HMR unless we include the module.hot code below.
 export function init() {
   document.getElementById("test2").innerText = "Init function run!";
 }
 
-document.getElementById("test3").innerText = "Dynamic.js side-effect!";
+// This side-effect should run each time dynamic.js changes in development mode.
+document.getElementById("test3").innerText = "dynamic.js side-effect!";
 
+// "module.hot" is injected by parcel in development mode.
+// This check will ensure that this code _only_ runs in this context
+// (e.g. it won't affect production)
 if (module.hot) {
-  module.hot.dispose(function (data) {
-    // module is about to be replaced.
-    // You can save data that should be accessible to the new asset in `data`
-    data.updated = Date.now();
-    console.log("module.hot.dispose callback invoked! from dynamic.js");
-  });
-
-  module.hot.accept(function (getParents) {
-    // module or one of its dependencies was just updated.
-    // data stored in `dispose` is available in `module.hot.data`
-    let { updated } = module.hot.data;
-    console.log("module.hot.accept callback invoked! from dynamic.js");
+  // The callback provided to "module.hot.accept" will be run whenever
+  // parcel sends a new version of `faqs.js` to the browser in development.
+  module.hot.accept(() => {
+    // Actually run the new version of your init function.
+    init();
   });
 }
