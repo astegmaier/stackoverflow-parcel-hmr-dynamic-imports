f3da133f48201a1e1880c45695f880c31e305c8e
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-11-03 11:14:48 -0400

    Add console logging

--- src/dynamic.js
+++ src/dynamic.js
@@ -1,3 +1,21 @@
 export function init() {
-    document.getElementById("test2").innerText = "Init function run!";
+  document.getElementById("test2").innerText = "Init function run!";
+}
+
+document.getElementById("test3").innerText = "Dynamic.js side-effect!";
+
+if (module.hot) {
+  module.hot.dispose(function (data) {
+    // module is about to be replaced.
+    // You can save data that should be accessible to the new asset in `data`
+    data.updated = Date.now();
+    console.log("module.hot.dispose callback invoked! from dynamic.js");
+  });
+
+  module.hot.accept(function (getParents) {
+    // module or one of its dependencies was just updated.
+    // data stored in `dispose` is available in `module.hot.data`
+    let { updated } = module.hot.data;
+    console.log("module.hot.accept callback invoked! from dynamic.js");
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 document.getElementById("test1").innerText = "main.js loaded!";
 
-import("./dynamic.js").then(function (stuff) {
-  stuff.init();
-});
+import("./dynamic.js")
+  .then(function (stuff) {
+    console.log("Dynamic import from main.js SUCCEEDED. Here's the stuff: ", stuff);
+    stuff.init();
+  })
+  .catch((e) => console.log("Dynamic import from main.js FAILED with this error: ", e));
+
+if (module.hot) {
+  module.hot.dispose(function (data) {
+    // module is about to be replaced.
+    // You can save data that should be accessible to the new asset in `data`
+    data.updated = Date.now();
+    console.log("module.hot.dispose callback invoked! from main.js");
+  });
+
+  module.hot.accept(function (getParents) {
+    // module or one of its dependencies was just updated.
+    // data stored in `dispose` is available in `module.hot.data`
+    let { updated } = module.hot.data;
+    console.log("module.hot.accept callback invoked! from main.js");
+  });
+}
